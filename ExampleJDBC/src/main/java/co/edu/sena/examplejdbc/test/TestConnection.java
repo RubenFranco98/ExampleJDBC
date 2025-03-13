8c51a747dd392cb249af6265f524f4dd4a3b8c6b
Ruben Felipe Franco Chaparro <ruben.franco1598@gmail.com>
2025-03-13 09:08:16 -0500

    Conexión JDBC
    Se crea clase de conexión a mysql. También se agrega clase para mensajería.

---
+++ ExampleJDBC/src/main/java/co/edu/sena/examplejdbc/test/TestConnection.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.sena.examplejdbc.test;
+
+import co.edu.sena.examplejdbc.bd.DBConnection;
+
+/**
+ * Date: 13/03/2025
+ * @author Ruben
+ * Objective: probar la conexión a la base de datos
+ */
+
+public class TestConnection {
+    public static void main(String[] args) {
+        DBConnection connection = new DBConnection();
+        connection.connect();
+        connection.disconnect();
+        }
+    }
